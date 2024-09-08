087e3ff5776a5f4da79a51e551898940da1f3bb5
2wndrhs <2wndrhs@gmail.com>
2024-09-09 01:49:45 +0900

    feat: off_t 타입 정의

--- project1/project1-xv6/types.h
+++ project1/project1-xv6/types.h
@@ -1,4 +1,8 @@
-typedef unsigned int   uint;
+typedef unsigned int uint;
 typedef unsigned short ushort;
-typedef unsigned char  uchar;
+typedef unsigned char uchar;
 typedef uint pde_t;
+
+#ifndef _OFF_T
+typedef unsigned int off_t;
+#endif
