9d56950b1ba4768011a8ff37b1c98df52caf579b
2wndrhs <2wndrhs@gmail.com>
2024-09-22 01:17:24 +0900

    fix: `_STDIO_H`가 정의되어 있지 않을 때만 `off_t` 타입을 정의하도록 수정

--- project1/project1-xv6/types.h
+++ project1/project1-xv6/types.h
@@ -3,7 +3,7 @@ typedef unsigned short ushort;
 typedef unsigned char uchar;
 typedef uint pde_t;
 
-// _OFF_T가 정의되어 있지 않다면 off_t를 int로 정의
-#ifndef _OFF_T
+// _STDIO_H가 정의되어 있지 않다면 off_t를 int로 정의
+#ifndef _STDIO_H
 typedef int off_t;
 #endif
