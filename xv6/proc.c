dc4bea1e5420646db5ac928af9d1a0f174ca3814
2wndrhs <2wndrhs@gmail.com>
2024-10-27 05:21:46 +0900

    docs: xv6 스케줄러 관련 함수 주석 추가

--- xv6/proc.c
+++ xv6/proc.c
@@ -7,7 +7,8 @@
 #include "proc.h"
 #include "spinlock.h"
 
-struct {
+struct
+{
   struct spinlock lock;
   struct proc proc[NPROC];
 } ptable;
@@ -20,57 +21,72 @@ extern void trapret(void);
 
 static void wakeup1(void *chan);
 
-void
-pinit(void)
+void pinit(void)
 {
   initlock(&ptable.lock, "ptable");
 }
 
 // Must be called with interrupts disabled
-int
-cpuid() {
-  return mycpu()-cpus;
+// 현재 실행 중인 CPU의 ID 반환
+int cpuid()
+{
+  return mycpu() - cpus;
 }
 
 // Must be called with interrupts disabled to avoid the caller being
 // rescheduled between reading lapicid and running through the loop.
-struct cpu*
+// 현재 실행 중인 CPU의 구조체 포인터 반환
+struct cpu *
 mycpu(void)
 {
   int apicid, i;
-  
-  if(readeflags()&FL_IF)
+
+  // 인터럽트가 활성화되어 있는지 확인
+  if (readeflags() & FL_IF)
     panic("mycpu called with interrupts enabled\n");
-  
+
+  // lapicid() 함수를 호출하여 현재 CPU의 APIC ID를 읽어옴
   apicid = lapicid();
+
   // APIC IDs are not guaranteed to be contiguous. Maybe we should have
   // a reverse map, or reserve a register to store &cpus[i].
-  for (i = 0; i < ncpu; ++i) {
+
+  // `cpus` 배열을 순회하며 APIC ID가 일치하는 CPU 구조체를 찾아 해당 구조체의 포인터를 반환
+  for (i = 0; i < ncpu; ++i)
+  {
     if (cpus[i].apicid == apicid)
       return &cpus[i];
   }
+
+  // 일치하는 APIC ID를 찾지 못하면 패닉 발생
   panic("unknown apicid\n");
 }
 
 // Disable interrupts so that we are not rescheduled
 // while reading proc from the cpu structure
-struct proc*
-myproc(void) {
+// 현재 CPU에서 실행 중인 프로세스의 구조체 포인터 반환
+struct proc *
+myproc(void)
+{
   struct cpu *c;
   struct proc *p;
+  // 인터럽트 비활성화 (함수 실행 중에 프로세스가 다시 스케줄링 되는 것을 방지)
   pushcli();
+  // 현재 실행 중인 CPU 구조체 포인터 얻기
   c = mycpu();
+  // 현재 CPU에서 실행 중인 프로세스의 구조체 포인터 얻기
   p = c->proc;
+  // 인터럽트 재활성화
   popcli();
   return p;
 }
 
-//PAGEBREAK: 32
-// Look in the process table for an UNUSED proc.
-// If found, change state to EMBRYO and initialize
-// state required to run in the kernel.
-// Otherwise return 0.
-static struct proc*
+// PAGEBREAK: 32
+//  Look in the process table for an UNUSED proc.
+//  If found, change state to EMBRYO and initialize
+//  state required to run in the kernel.
+//  Otherwise return 0.
+static struct proc *
 allocproc(void)
 {
   struct proc *p;
@@ -78,8 +94,8 @@ allocproc(void)
 
   acquire(&ptable.lock);
 
-  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
-    if(p->state == UNUSED)
+  for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+    if (p->state == UNUSED)
       goto found;
 
   release(&ptable.lock);
@@ -92,7 +108,8 @@ found:
   release(&ptable.lock);
 
   // Allocate kernel stack.
-  if((p->kstack = kalloc()) == 0){
+  if ((p->kstack = kalloc()) == 0)
+  {
     p->state = UNUSED;
     return 0;
   }
@@ -100,33 +117,32 @@ found:
 
   // Leave room for trap frame.
   sp -= sizeof *p->tf;
-  p->tf = (struct trapframe*)sp;
+  p->tf = (struct trapframe *)sp;
 
   // Set up new context to start executing at forkret,
   // which returns to trapret.
   sp -= 4;
-  *(uint*)sp = (uint)trapret;
+  *(uint *)sp = (uint)trapret;
 
   sp -= sizeof *p->context;
-  p->context = (struct context*)sp;
+  p->context = (struct context *)sp;
   memset(p->context, 0, sizeof *p->context);
   p->context->eip = (uint)forkret;
 
   return p;
 }
 
-//PAGEBREAK: 32
-// Set up first user process.
-void
-userinit(void)
+// PAGEBREAK: 32
+//  Set up first user process.
+void userinit(void)
 {
   struct proc *p;
   extern char _binary_initcode_start[], _binary_initcode_size[];
 
   p = allocproc();
-  
+
   initproc = p;
-  if((p->pgdir = setupkvm()) == 0)
+  if ((p->pgdir = setupkvm()) == 0)
     panic("userinit: out of memory?");
   inituvm(p->pgdir, _binary_initcode_start, (int)_binary_initcode_size);
   p->sz = PGSIZE;
@@ -137,7 +153,7 @@ userinit(void)
   p->tf->ss = p->tf->ds;
   p->tf->eflags = FL_IF;
   p->tf->esp = PGSIZE;
-  p->tf->eip = 0;  // beginning of initcode.S
+  p->tf->eip = 0; // beginning of initcode.S
 
   safestrcpy(p->name, "initcode", sizeof(p->name));
   p->cwd = namei("/");
@@ -155,18 +171,20 @@ userinit(void)
 
 // Grow current process's memory by n bytes.
 // Return 0 on success, -1 on failure.
-int
-growproc(int n)
+int growproc(int n)
 {
   uint sz;
   struct proc *curproc = myproc();
 
   sz = curproc->sz;
-  if(n > 0){
-    if((sz = allocuvm(curproc->pgdir, sz, sz + n)) == 0)
+  if (n > 0)
+  {
+    if ((sz = allocuvm(curproc->pgdir, sz, sz + n)) == 0)
       return -1;
-  } else if(n < 0){
-    if((sz = deallocuvm(curproc->pgdir, sz, sz + n)) == 0)
+  }
+  else if (n < 0)
+  {
+    if ((sz = deallocuvm(curproc->pgdir, sz, sz + n)) == 0)
       return -1;
   }
   curproc->sz = sz;
@@ -177,20 +195,21 @@ growproc(int n)
 // Create a new process copying p as the parent.
 // Sets up stack to return as if from system call.
 // Caller must set state of returned proc to RUNNABLE.
-int
-fork(void)
+int fork(void)
 {
   int i, pid;
   struct proc *np;
   struct proc *curproc = myproc();
 
   // Allocate process.
-  if((np = allocproc()) == 0){
+  if ((np = allocproc()) == 0)
+  {
     return -1;
   }
 
   // Copy process state from proc.
-  if((np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0){
+  if ((np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0)
+  {
     kfree(np->kstack);
     np->kstack = 0;
     np->state = UNUSED;
@@ -203,8 +222,8 @@ fork(void)
   // Clear %eax so that fork returns 0 in the child.
   np->tf->eax = 0;
 
-  for(i = 0; i < NOFILE; i++)
-    if(curproc->ofile[i])
+  for (i = 0; i < NOFILE; i++)
+    if (curproc->ofile[i])
       np->ofile[i] = filedup(curproc->ofile[i]);
   np->cwd = idup(curproc->cwd);
 
@@ -224,19 +243,20 @@ fork(void)
 // Exit the current process.  Does not return.
 // An exited process remains in the zombie state
 // until its parent calls wait() to find out it exited.
-void
-exit(void)
+void exit(void)
 {
   struct proc *curproc = myproc();
   struct proc *p;
   int fd;
 
-  if(curproc == initproc)
+  if (curproc == initproc)
     panic("init exiting");
 
   // Close all open files.
-  for(fd = 0; fd < NOFILE; fd++){
-    if(curproc->ofile[fd]){
+  for (fd = 0; fd < NOFILE; fd++)
+  {
+    if (curproc->ofile[fd])
+    {
       fileclose(curproc->ofile[fd]);
       curproc->ofile[fd] = 0;
     }
@@ -253,10 +273,12 @@ exit(void)
   wakeup1(curproc->parent);
 
   // Pass abandoned children to init.
-  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-    if(p->parent == curproc){
+  for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+  {
+    if (p->parent == curproc)
+    {
       p->parent = initproc;
-      if(p->state == ZOMBIE)
+      if (p->state == ZOMBIE)
         wakeup1(initproc);
     }
   }
@@ -269,22 +291,24 @@ exit(void)
 
 // Wait for a child process to exit and return its pid.
 // Return -1 if this process has no children.
-int
-wait(void)
+int wait(void)
 {
   struct proc *p;
   int havekids, pid;
   struct proc *curproc = myproc();
-  
+
   acquire(&ptable.lock);
-  for(;;){
+  for (;;)
+  {
     // Scan through table looking for exited children.
     havekids = 0;
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->parent != curproc)
+    for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+    {
+      if (p->parent != curproc)
         continue;
       havekids = 1;
-      if(p->state == ZOMBIE){
+      if (p->state == ZOMBIE)
+      {
         // Found one.
         pid = p->pid;
         kfree(p->kstack);
@@ -301,57 +325,69 @@ wait(void)
     }
 
     // No point waiting if we don't have any children.
-    if(!havekids || curproc->killed){
+    if (!havekids || curproc->killed)
+    {
       release(&ptable.lock);
       return -1;
     }
 
     // Wait for children to exit.  (See wakeup1 call in proc_exit.)
-    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+    sleep(curproc, &ptable.lock); // DOC: wait-sleep
   }
 }
 
-//PAGEBREAK: 42
-// Per-CPU process scheduler.
-// Each CPU calls scheduler() after setting itself up.
-// Scheduler never returns.  It loops, doing:
-//  - choose a process to run
-//  - swtch to start running that process
-//  - eventually that process transfers control
-//      via swtch back to the scheduler.
-void
-scheduler(void)
+// PAGEBREAK: 42
+//  Per-CPU process scheduler.
+//  Each CPU calls scheduler() after setting itself up.
+//  Scheduler never returns.  It loops, doing:
+//   - choose a process to run
+//   - swtch to start running that process
+//   - eventually that process transfers control
+//       via swtch back to the scheduler.
+
+// 무한 루프를 돌면서 실행 가능한 프로세스를 찾아 실행
+void scheduler(void)
 {
   struct proc *p;
   struct cpu *c = mycpu();
   c->proc = 0;
-  
-  for(;;){
+
+  for (;;)
+  {
     // Enable interrupts on this processor.
     sti();
 
     // Loop over process table looking for process to run.
+    // acquire() 함수를 호출하여 프로세스 테이블 락 획득
     acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
+    // ptable.proc 배열을 순회하면서 프로세스의 상태가 RUNNABLE인 프로세스를 찾음
+    for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+    {
+      if (p->state != RUNNABLE)
         continue;
 
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
+      c->proc = p;        // 현재 CPU에서 실행할 프로세스를 설정
+      switchuvm(p);       // 사용자 가상 메모리 공간으로 전환
+      p->state = RUNNING; // 프로세스의 상태를 RUNNING으로 변경
 
+      // swtch() 함수를 호출하여 현재 CPU 컨텍스트를 저장하고,
+      // 선택된 프로세스의 컨텍스트로 전환
       swtch(&(c->scheduler), p->context);
+
+      // 프로세스가 실행을 마치고 다시 스케줄러로 돌아오면
+      // 커널 가상 메모리 공간으로 전환
       switchkvm();
 
       // Process is done running for now.
       // It should have changed its p->state before coming back.
       c->proc = 0;
     }
-    release(&ptable.lock);
 
+    // 프로세스 테이블 락 해제
+    release(&ptable.lock);
   }
 }
 
@@ -362,45 +398,63 @@ scheduler(void)
 // be proc->intena and proc->ncli, but that would
 // break in the few places where a lock is held but
 // there's no process.
-void
-sched(void)
+// 현재 실행 중인 프로세스를 스케줄러로 넘기고, 다른 프로세스를 실행할 수 있도록 함
+void sched(void)
 {
   int intena;
+  // myproc() 함수를 호출하여 현재 실행 중인 프로세스의 구조체 포인터 획득
   struct proc *p = myproc();
 
-  if(!holding(&ptable.lock))
+  // 프로세스 테이블 락이 획득되었는지 확인
+  if (!holding(&ptable.lock))
     panic("sched ptable.lock");
-  if(mycpu()->ncli != 1)
+  // 인터럽트 비활성화 횟수가 1인지 확인
+  if (mycpu()->ncli != 1)
     panic("sched locks");
-  if(p->state == RUNNING)
+  // 현재 프로세스의 상태가 RUNNING인지 확인
+  // 프로세스가 실행 중인 상태에서 스케줄러로 전환되는 것을 방지
+  if (p->state == RUNNING)
     panic("sched running");
-  if(readeflags()&FL_IF)
+  // 인터럽트가 활성화되어 있는지 확인
+  // 스케줄러로 전환되는 동안 인터럽트가 발생하지 않도록 함
+  if (readeflags() & FL_IF)
     panic("sched interruptible");
+  // 현재 CPU의 인터럽트 상태를 저장
+  // 스케줄러로 전환한 후에 인터럽트 상태를 복원하기 위함
   intena = mycpu()->intena;
+  // 현재 프로세스의 컨텍스트를 저장하고 스케줄러의 컨텍스트로 전환
   swtch(&p->context, mycpu()->scheduler);
+  // 스케줄러가 다시 원래의 프로세스를 실행하기로 결정하면
+  // 이전에 저장된 프로세스의 컨텍스트로 복원됨
   mycpu()->intena = intena;
 }
 
 // Give up the CPU for one scheduling round.
-void
-yield(void)
+// 현재 실행 중인 프로세스가 CPU를 포기하고
+// 다른 프로세스가 실행될 수 있도록 함
+void yield(void)
 {
-  acquire(&ptable.lock);  //DOC: yieldlock
+  // 프로세스 테이블 락 획득 (데이터 일관성 유지)
+  acquire(&ptable.lock); // DOC: yieldlock
+  // myproc() 함수를 호출하여 현재 실행중인 프로세스의 구조체 포인터 획득
+  // 현재 프로세스의 상태를 RUNNABLE로 변경 (프로세스가 실행 가능한 상태암을 나타냄)
   myproc()->state = RUNNABLE;
+  // sched() 함수를 호출하여 스케줄러로 제어를 넘김
   sched();
+  // 프로세스 테이블 락 해제
   release(&ptable.lock);
 }
 
 // A fork child's very first scheduling by scheduler()
 // will swtch here.  "Return" to user space.
-void
-forkret(void)
+void forkret(void)
 {
   static int first = 1;
   // Still holding ptable.lock from scheduler.
   release(&ptable.lock);
 
-  if (first) {
+  if (first)
+  {
     // Some initialization functions must be run in the context
     // of a regular process (e.g., they call sleep), and thus cannot
     // be run from main().
@@ -414,15 +468,17 @@ forkret(void)
 
 // Atomically release lock and sleep on chan.
 // Reacquires lock when awakened.
-void
-sleep(void *chan, struct spinlock *lk)
+// 현재 실행 중인 프로세스를 지정된 채널에서 잠들게 하고,
+// 다른 프로세스가 실행될 수 있도록 함
+void sleep(void *chan, struct spinlock *lk)
 {
+  // 현재 실행 중인 프로세스의 구조체 포인터 획득
   struct proc *p = myproc();
-  
-  if(p == 0)
+
+  if (p == 0)
     panic("sleep");
 
-  if(lk == 0)
+  if (lk == 0)
     panic("sleep without lk");
 
   // Must acquire ptable.lock in order to
@@ -431,62 +487,77 @@ sleep(void *chan, struct spinlock *lk)
   // guaranteed that we won't miss any wakeup
   // (wakeup runs with ptable.lock locked),
   // so it's okay to release lk.
-  if(lk != &ptable.lock){  //DOC: sleeplock0
-    acquire(&ptable.lock);  //DOC: sleeplock1
+
+  // 제공된 락이 ptable.lock이 아닌 경우,
+  // ptable.lock을 획득하고 제공된 락을 해제
+  if (lk != &ptable.lock)
+  {                        // DOC: sleeplock0
+    acquire(&ptable.lock); // DOC: sleeplock1
     release(lk);
   }
   // Go to sleep.
-  p->chan = chan;
-  p->state = SLEEPING;
+  p->chan = chan;      // 프로세스의 chan 필드를 지정된 채널로 설정
+  p->state = SLEEPING; // 프로세스의 상태를 SLEEPING으로 변경
 
+  // sched() 함수를 호출하여 현재 프로세스를 스케줄러로 넘기고,
+  // 다른 프로세스를 실행할 수 있도록 함
+  // sched() 함수는 내부적으로 swtch() 함수를 호출하여 스케줄러 컨텍스트로 전환
   sched();
 
+  // 프로세스가 깨어나면, `chan` 필드를 0으로 설정하여 초기화
   // Tidy up.
   p->chan = 0;
 
   // Reacquire original lock.
-  if(lk != &ptable.lock){  //DOC: sleeplock2
+  // ptable.lock이 아닌 락을 제공한 경우,
+  // ptable.lock을 해제하고 제공된 락을 다시 획득
+  if (lk != &ptable.lock)
+  { // DOC: sleeplock2
     release(&ptable.lock);
     acquire(lk);
   }
 }
 
-//PAGEBREAK!
-// Wake up all processes sleeping on chan.
-// The ptable lock must be held.
+// PAGEBREAK!
+//  Wake up all processes sleeping on chan.
+//  The ptable lock must be held.
 static void
 wakeup1(void *chan)
 {
   struct proc *p;
-
-  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
-    if(p->state == SLEEPING && p->chan == chan)
-      p->state = RUNNABLE;
+  // 프로세스 테이블을 순회하면서 지정된 채널에서 잠들고 있는 모든 프로세스를 찾음
+  for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+    if (p->state == SLEEPING && p->chan == chan)
+      p->state = RUNNABLE; // 프로세스의 상태를 RUNNABLE로 변경
 }
 
 // Wake up all processes sleeping on chan.
-void
-wakeup(void *chan)
+// 지정된 채널에서 잠들고 있는 모든 프로세스를 깨움
+void wakeup(void *chan)
 {
+  // 프로세스 테이블 락 획득
   acquire(&ptable.lock);
+  // wakeup1() 함수를 호출하여 지정된 채널에서 잠들고 있는 모든 프로세스를 깨움
   wakeup1(chan);
+  // 프로세스 테이블 락 해제
   release(&ptable.lock);
 }
 
 // Kill the process with the given pid.
 // Process won't exit until it returns
 // to user space (see trap in trap.c).
-int
-kill(int pid)
+int kill(int pid)
 {
   struct proc *p;
 
   acquire(&ptable.lock);
-  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-    if(p->pid == pid){
+  for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+  {
+    if (p->pid == pid)
+    {
       p->killed = 1;
       // Wake process from sleep if necessary.
-      if(p->state == SLEEPING)
+      if (p->state == SLEEPING)
         p->state = RUNNABLE;
       release(&ptable.lock);
       return 0;
@@ -496,37 +567,37 @@ kill(int pid)
   return -1;
 }
 
-//PAGEBREAK: 36
-// Print a process listing to console.  For debugging.
-// Runs when user types ^P on console.
-// No lock to avoid wedging a stuck machine further.
-void
-procdump(void)
+// PAGEBREAK: 36
+//  Print a process listing to console.  For debugging.
+//  Runs when user types ^P on console.
+//  No lock to avoid wedging a stuck machine further.
+void procdump(void)
 {
   static char *states[] = {
-  [UNUSED]    "unused",
-  [EMBRYO]    "embryo",
-  [SLEEPING]  "sleep ",
-  [RUNNABLE]  "runble",
-  [RUNNING]   "run   ",
-  [ZOMBIE]    "zombie"
-  };
+      [UNUSED] "unused",
+      [EMBRYO] "embryo",
+      [SLEEPING] "sleep ",
+      [RUNNABLE] "runble",
+      [RUNNING] "run   ",
+      [ZOMBIE] "zombie"};
   int i;
   struct proc *p;
   char *state;
   uint pc[10];
 
-  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-    if(p->state == UNUSED)
+  for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+  {
+    if (p->state == UNUSED)
       continue;
-    if(p->state >= 0 && p->state < NELEM(states) && states[p->state])
+    if (p->state >= 0 && p->state < NELEM(states) && states[p->state])
       state = states[p->state];
     else
       state = "???";
     cprintf("%d %s %s", p->pid, state, p->name);
-    if(p->state == SLEEPING){
-      getcallerpcs((uint*)p->context->ebp+2, pc);
-      for(i=0; i<10 && pc[i] != 0; i++)
+    if (p->state == SLEEPING)
+    {
+      getcallerpcs((uint *)p->context->ebp + 2, pc);
+      for (i = 0; i < 10 && pc[i] != 0; i++)
         cprintf(" %p", pc[i]);
     }
     cprintf("\n");
--- xv6/spinlock.c
+++ xv6/spinlock.c
@@ -9,8 +9,7 @@
 #include "proc.h"
 #include "spinlock.h"
 
-void
-initlock(struct spinlock *lk, char *name)
+void initlock(struct spinlock *lk, char *name)
 {
   lk->name = name;
   lk->locked = 0;
@@ -21,15 +20,14 @@ initlock(struct spinlock *lk, char *name)
 // Loops (spins) until the lock is acquired.
 // Holding a lock for a long time may cause
 // other CPUs to waste time spinning to acquire it.
-void
-acquire(struct spinlock *lk)
+void acquire(struct spinlock *lk)
 {
   pushcli(); // disable interrupts to avoid deadlock.
-  if(holding(lk))
+  if (holding(lk))
     panic("acquire");
 
   // The xchg is atomic.
-  while(xchg(&lk->locked, 1) != 0)
+  while (xchg(&lk->locked, 1) != 0)
     ;
 
   // Tell the C compiler and the processor to not move loads or stores
@@ -39,16 +37,17 @@ acquire(struct spinlock *lk)
 
   // Record info about lock acquisition for debugging.
   lk->cpu = mycpu();
+  // 호출 스택 정보를 기록
   getcallerpcs(&lk, lk->pcs);
 }
 
 // Release the lock.
-void
-release(struct spinlock *lk)
+void release(struct spinlock *lk)
 {
-  if(!holding(lk))
+  if (!holding(lk))
     panic("release");
 
+  // 락 획득 정보 초기화
   lk->pcs[0] = 0;
   lk->cpu = 0;
 
@@ -62,32 +61,32 @@ release(struct spinlock *lk)
   // Release the lock, equivalent to lk->locked = 0.
   // This code can't use a C assignment, since it might
   // not be atomic. A real OS would use C atomics here.
-  asm volatile("movl $0, %0" : "+m" (lk->locked) : );
+  asm volatile("movl $0, %0" : "+m"(lk->locked) :);
 
+  // 인터럽트 재활성화
   popcli();
 }
 
 // Record the current call stack in pcs[] by following the %ebp chain.
-void
-getcallerpcs(void *v, uint pcs[])
+void getcallerpcs(void *v, uint pcs[])
 {
   uint *ebp;
   int i;
 
-  ebp = (uint*)v - 2;
-  for(i = 0; i < 10; i++){
-    if(ebp == 0 || ebp < (uint*)KERNBASE || ebp == (uint*)0xffffffff)
+  ebp = (uint *)v - 2;
+  for (i = 0; i < 10; i++)
+  {
+    if (ebp == 0 || ebp < (uint *)KERNBASE || ebp == (uint *)0xffffffff)
       break;
-    pcs[i] = ebp[1];     // saved %eip
-    ebp = (uint*)ebp[0]; // saved %ebp
+    pcs[i] = ebp[1];      // saved %eip
+    ebp = (uint *)ebp[0]; // saved %ebp
   }
-  for(; i < 10; i++)
+  for (; i < 10; i++)
     pcs[i] = 0;
 }
 
 // Check whether this cpu is holding the lock.
-int
-holding(struct spinlock *lock)
+int holding(struct spinlock *lock)
 {
   int r;
   pushcli();
@@ -96,31 +95,37 @@ holding(struct spinlock *lock)
   return r;
 }
 
-
 // Pushcli/popcli are like cli/sti except that they are matched:
 // it takes two popcli to undo two pushcli.  Also, if interrupts
 // are off, then pushcli, popcli leaves them off.
 
-void
-pushcli(void)
+// 인터럽트를 비활성화
+void pushcli(void)
 {
   int eflags;
-
+  // readeflags() 함수를 호출하여 현재 CPU 상태 레지스터 값을 읽어옴
   eflags = readeflags();
+  // cli() 어셈블리 명령어를 호출하여 인터럽트 비활성화
   cli();
-  if(mycpu()->ncli == 0)
+  // 현재 CPU에서 인터럽트가 처음으로 비활성화되는 경우,
+  // mycpu()->intena 변수에 이전 인터럽트 상태를 저장
+  if (mycpu()->ncli == 0)
     mycpu()->intena = eflags & FL_IF;
+  // 인터럽트 비활성화 횟수 증가
   mycpu()->ncli += 1;
 }
 
-void
-popcli(void)
+// 인터럽트 활성화
+void popcli(void)
 {
-  if(readeflags()&FL_IF)
+  // 현재 인터럽트가 활성화되어 있는지 확인
+  if (readeflags() & FL_IF)
     panic("popcli - interruptible");
-  if(--mycpu()->ncli < 0)
+  // 인터럽트 비활성화 횟수 감소
+  if (--mycpu()->ncli < 0)
+    // 인터럽트 비활성화 횟수가 음수가 되면 패닉 발생
     panic("popcli");
-  if(mycpu()->ncli == 0 && mycpu()->intena)
+  // 인터럽트 비활성화 횟수가 0이 되면 인터럽트 활성화
+  if (mycpu()->ncli == 0 && mycpu()->intena)
     sti();
 }
-
