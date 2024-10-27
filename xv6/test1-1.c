6e2941770e3e860fb34dfc85b2c46e31b8d56ff8
2wndrhs <2wndrhs@gmail.com>
2024-10-28 05:03:51 +0900

    feat: MLFQ 테스트 프로그램 추가

---
+++ xv6/test1-1.c
@@ -0,0 +1,37 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(void)
+{
+    printf(1, "start scheduler_test\n");
+
+    int pid = fork();
+    if (pid < 0)
+    {
+        printf(1, "fork failed\n");
+        exit();
+    }
+
+    if (pid == 0)
+    {
+        // Child process
+        if (set_proc_info(0, 0, 0, 0, 500) < 0)
+        {
+            printf(1, "set_proc_info failed\n");
+            exit();
+        }
+
+        while (1)
+        {
+        }
+    }
+    else
+    {
+        // wait for child process to finish
+        wait();
+    }
+
+    printf(1, "end of scheduler_test\n");
+    exit();
+}
---
+++ xv6/test1-3.c
@@ -0,0 +1,44 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(void)
+{
+    printf(1, "start scheduler_test\n");
+
+    int pid;
+    int child_count = 3; // 생성할 자식 프로세스 수
+
+    for (int i = 0; i < child_count; i++)
+    {
+        pid = fork();
+        if (pid < 0)
+        {
+            printf(1, "fork failed\n");
+            exit();
+        }
+
+        if (pid == 0)
+        {
+            // Child process
+            if (set_proc_info(2, 0, 0, 0, 300) < 0)
+            {
+                printf(1, "set_proc_info failed\n");
+                exit();
+            }
+
+            while (1)
+            {
+            }
+        }
+    }
+
+    for (int i = 0; i < child_count; i++)
+    {
+        wait();
+    }
+
+    printf(1, "end of scheduler_test\n");
+
+    exit();
+}
